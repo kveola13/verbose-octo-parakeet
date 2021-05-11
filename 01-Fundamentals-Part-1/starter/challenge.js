3604f4fb02b8c8395ad7fd216b95135750ddd0dc
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2021-05-11 11:12:46 +0200

    challenge 1 complete

--- 01-Fundamentals-Part-1/starter/challenge.js
+++ 01-Fundamentals-Part-1/starter/challenge.js
@@ -6,14 +6,27 @@ let frodo = {
   weight: 95,
   height: 1.88,
 };
+let aragorn = {
+  weight: 92,
+  height: 1.95,
+};
+let legolas = {
+  weight: 85,
+  height: 1.76,
+};
 
 function calculateBodyMassIndex(weight, height) {
-  return weight / (height * height);
+  return weight / height ** 2;
 }
 
 let bilboBmi = calculateBodyMassIndex(bilbo.weight, bilbo.height).toFixed(2);
 let frodoBmi = calculateBodyMassIndex(frodo.weight, frodo.height).toFixed(2);
-console.log(`Biblo bmi: ${bilboBmi}\nFrodo bmi: ${frodoBmi}`);
+let aragornBmi = calculateBodyMassIndex(aragorn.weight, aragorn.height).toFixed(2);
+let legolasBmi = calculateBodyMassIndex(legolas.weight, legolas.height).toFixed(2);
+
+console.log(
+  `Biblo bmi: ${bilboBmi}\nFrodo bmi: ${frodoBmi}\naragorn bmi: ${aragornBmi}\nLegolas bmi:${legolasBmi}`
+);
 
 let bilboHigherBmiThanFrodo = bilboBmi > frodoBmi;
 console.log(
