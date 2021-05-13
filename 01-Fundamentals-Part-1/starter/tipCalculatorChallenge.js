0bce7237064348e9296d70730b214005c8236d27
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2021-05-13 11:56:22 +0200

    Tip calculator challenge

--- 01-Fundamentals-Part-1/starter/tipCalculatorChallenge.js
+++ 01-Fundamentals-Part-1/starter/tipCalculatorChallenge.js
@@ -4,7 +4,7 @@ const bigTip = 20
 let bills = [275, 40, 430]
 
 bills.forEach(bill => {
-    let billIsInRange = 50 <= bill <= 300
+    let billIsInRange = bill >= 50 && bill <= 300
     
     console.log(`Bill: ${bill}, tip is ${billIsInRange ? (bill / 100) * tip : (bill/100) * bigTip}
 The total value: ${billIsInRange ? bill + (bill / 100) * tip : bill + (bill / 100) * bigTip}`);
