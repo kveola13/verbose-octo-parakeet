ec639183c4888ee29a86c1048c9288f50b55cfa7
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2021-05-15 15:13:21 +0200

    added script

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,38 @@
-// Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+let syndicate = {
+  name: "Syndicate",
+  scores: [96, 108, 89],
+};
+let nilfgaard = {
+  name: "Nilfgaard",
+  scores: [88, 91, 110],
+};
+let northernRealms = {
+  name: "Northern Realms",
+  scores: [97, 112, 101],
+};
+let scoiaTael = {
+  name: "Scoia'tael",
+  scores: [109, 95, 123],
+};
+let monsters = {
+  name: "Monsters",
+  scores: [97, 112, 101],
+};
+let skellige = {
+  name: "Skellige",
+  scores: [109, 95, 106],
+};
+
+console.log("Testing");
+console.warn("Testing...");
+console.error("Testing!");
+console.table([
+  syndicate,
+  skellige,
+  nilfgaard,
+  monsters,
+  northernRealms,
+  scoiaTael,
+]);
