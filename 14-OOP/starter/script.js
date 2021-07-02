aafc6a0b3c1a14f16ebfb26d80a07c2ad35919ea
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2021-07-02 15:39:54 +0200

    added script

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,20 @@
-'use strict';
+"use strict";
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const ola = new Person("Ola", 1992);
+console.log(ola);
+const jens = new Person("Jens", 2017);
+const john = new Person("John", 1920);
+console.log(jens, john);
+
+console.log(ola instanceof Person);
+
+Person.prototype.calcAge = function () {
+  console.log(2021 - this.birthYear);
+};
+
+ola.calcAge();
