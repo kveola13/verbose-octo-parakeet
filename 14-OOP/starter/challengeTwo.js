1116bcd950af6810d5ba0f23f7898cabba8bd414
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2021-07-06 10:48:36 +0200

    added challenge two

---
+++ 14-OOP/starter/challengeTwo.js
@@ -0,0 +1,33 @@
+"use strict";
+
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed += 10;
+    console.log(this.speed);
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(this.speed);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new Car("Ford", 120);
+ford.accelerate();
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+console.log(ford.speedUS);
+ford.speedUS = 50;
+console.log(ford);
