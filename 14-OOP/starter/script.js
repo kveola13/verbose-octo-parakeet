6094d7e3a768968ddcca409aa466e1ed3ad2fb1e
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2021-07-06 10:48:42 +0200

    added script

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -32,6 +32,17 @@ class PersonClass {
   greet() {
     console.log(`Hey ${this.firstName}`);
   }
+  get age() {
+    return 2021 - this.birthYear;
+  }
+  set fullName(name) {
+    if (name.includes(" ")) this._fullName = name;
+    else console.log(`${name} is not a full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
 const morty = new PersonClass("Morty", 1997);
@@ -44,6 +55,20 @@ const account = {
   get latest() {
     return this.movements.slice(-1).pop();
   },
+  set latest(amount) {
+    return this.movements.push(amount);
+  },
 };
 
 console.log(account.latest);
+
+const PersonProto = {
+  calcAge() {
+    console.log(2021 - this.birthYear);
+  },
+};
+
+const rick = Object.create(PersonProto);
+rick.name = "Rick";
+rick.birthYear = "1936";
+rick.calcAge();
