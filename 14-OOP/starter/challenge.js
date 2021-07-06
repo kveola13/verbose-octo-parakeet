00fefc455774917b5fae0d8fdc147220fa20ae65
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2021-07-06 09:45:25 +0200

    added challenge

---
+++ 14-OOP/starter/challenge.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+Car.prototype.break = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const bmw = new Car("BMW", 120);
+const mercedes = new Car("Mercedes", 130);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.accelerate();
+bmw.accelerate();
+bmw.break();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.break();
