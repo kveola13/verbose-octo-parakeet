27070e95abbaab1b3902acec389507afa1f2af87
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2021-08-02 10:40:20 +0200

    added script

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -35,8 +35,19 @@ class Cycling extends Workout {
   constructor(coords, distance, duration, elevationGain) {
     super(coords, distance, duration);
     this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
   }
 }
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycling1 = new Cycling([39, -12], 27, 95, 523);
+
+console.log(run1, cycling1);
 
 class App {
   #map;
