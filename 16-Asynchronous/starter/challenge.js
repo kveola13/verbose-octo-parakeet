c65f4e3a875c1de2fa143266003b54fe1f2c503b
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2021-08-05 09:09:09 +0200

    added challenge

--- 16-Asynchronous/starter/challenge.js
+++ 16-Asynchronous/starter/challenge.js
@@ -1,6 +1,20 @@
 "use strict";
-const whereAmI = function (coords) {
-  console.log(coords);
+const whereAmI = function (lat, lng) {
+  console.log(lat, lng);
+  const location = `https://geocode.xyz/${lat},${lng}?geoit=json`;
+  fetch(location)
+    .then((response) => {
+      if (!response.ok)
+        throw new Error(`Problem with geocode, HTTP ${response.status}`);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+    })
+    .catch((error) => console.log(`${error.message}`));
 };
-const latlng = [];
-whereAmI(latlng);
+const [lat, lng] = [52.508, 13.381];
+//const [lat, lng] = [19.037, 72.873];
+//const [lat, lng] = [-33.933, 18.474];
+whereAmI(lat, lng);
